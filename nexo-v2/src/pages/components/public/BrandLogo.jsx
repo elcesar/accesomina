613d83d3f2c65abddf8446270796ab0d31a26c1c
elcesar <capintof@hotmail.com>
2026-09-06 22:26:14 -0300

    Delete nexo-v2/src/pages/components/public directory

--- nexo-v2/src/pages/components/public/BrandLogo.jsx
+++
@@ -1,9 +0,0 @@
-export default function BrandLogo({ variant = 'color', claim = true, className = '' }) {
-  const file = variant === 'white'
-    ? 'NK-blanco-horizontal.svg'
-    : claim
-      ? 'NK-color-horizontal-claim.svg'
-      : 'NK-color-horizontal.svg'
-
-  return <img className={`nk-brand-logo ${className}`} src={`/brand/${file}`} alt="Nexo Klar" />
-}
