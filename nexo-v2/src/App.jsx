255ae076cb4e73e73133114aba49a5cd477fce02
elcesar <capintof@hotmail.com>
2026-09-08 10:21:42 -0300

    feat: route control center dashboard and alerts

--- nexo-v2/src/App.jsx
+++ nexo-v2/src/App.jsx
@@ -3,6 +3,8 @@ import { AuthProvider, useAuth } from './services/auth.jsx'
 import AppLayout from './components/layout/AppLayout.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
+import AlertasPage from './pages/AlertasPage.jsx'
 import TrabajadoresPage from './pages/TrabajadoresPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import NuevoTrabajadorPage from './pages/NuevoTrabajadorPage.jsx'
@@ -53,6 +55,8 @@ export default function App() {
           <Route path="/app" element={
             <ProtectedRoute><AppLayout /></ProtectedRoute>
           }>
+            <Route index element={<DashboardPage />} />
+            <Route path="alertas" element={<AlertasPage />} />
             <Route path="trabajadores" element={<TrabajadoresPage />} />
             <Route path="trabajadores/nuevo" element={<NuevoTrabajadorPage />} />
             <Route path="trabajadores/:id" element={<FichaTrabajadorPage />} />
