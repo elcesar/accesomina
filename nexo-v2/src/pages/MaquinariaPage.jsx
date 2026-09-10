d83f8482edf938eea8d97c0d0ae51ad5417eaf36
elcesar <capintof@hotmail.com>
2026-09-10 11:22:07 -0300

    Specialize machinery inventory page

---
+++ nexo-v2/src/pages/MaquinariaPage.jsx
@@ -0,0 +1,5 @@
+import InventoryCategoryPage from '../components/inventory/InventoryCategoryPage.jsx'
+
+export default function MaquinariaPage() {
+  return <InventoryCategoryPage category="maquinaria" title="Maquinaria" description="Administra maquinaria, disponibilidad, mantenimientos y documentos desde el catálogo central de inventario." singular="Maquinaria" defaultType="Maquinaria" focus="machinery" />
+}
