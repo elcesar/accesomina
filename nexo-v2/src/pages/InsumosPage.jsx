6e152a65e7f03993b5d91f42447a576a958d9bef
elcesar <capintof@hotmail.com>
2026-09-10 12:52:45 -0300

    Use specialized consumables inventory page

--- nexo-v2/src/pages/InsumosPage.jsx
+++ nexo-v2/src/pages/InsumosPage.jsx
@@ -1,5 +1,5 @@
-import InventoryCategoryPage from '../components/inventory/InventoryCategoryPage.jsx'
+import ConsumablesInventoryPage from '../components/inventory/ConsumablesInventoryPage.jsx'
 
 export default function InsumosPage() {
-  return <InventoryCategoryPage category="insumos" title="Insumos y consumibles" description="Controla consumibles, stock disponible, mínimos y reposición desde el catálogo central de inventario." singular="Insumo" defaultType="Insumo" focus="stock" />
+  return <ConsumablesInventoryPage />
 }
