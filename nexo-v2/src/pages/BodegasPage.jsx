334c2e61d3ba4d3b38160e2fecb329ed2db9b823
elcesar <capintof@hotmail.com>
2026-09-10 11:44:26 -0300

    feat(inventory): add warehouses page

---
+++ nexo-v2/src/pages/BodegasPage.jsx
@@ -0,0 +1,5 @@
+import InventoryOperationsPage from '../components/inventory/InventoryOperationsPage.jsx'
+
+export default function BodegasPage() {
+  return <InventoryOperationsPage mode="warehouses" title="Bodegas y almacenes" description="Administra bodegas, ubicaciones internas, responsables y existencias distribuidas por ubicación." />
+}
