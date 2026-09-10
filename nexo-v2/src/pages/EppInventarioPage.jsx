d5b39d6c2448f5a03dc01f25d2fbf45cb5cbb43c
elcesar <capintof@hotmail.com>
2026-09-10 11:35:17 -0300

    Fase 8: especializar inventario EPP sobre inventario central

---
+++ nexo-v2/src/pages/EppInventarioPage.jsx
@@ -0,0 +1,5 @@
+import InventoryCategoryPage from '../components/inventory/InventoryCategoryPage.jsx'
+
+export default function EppInventarioPage() {
+  return <InventoryCategoryPage category="epp" title="EPP y protección personal" description="Controla existencias, tallas, vida útil y reposición de equipos de protección personal desde el catálogo central de inventario." singular="EPP" defaultType="EPP" focus="epp" />
+}
