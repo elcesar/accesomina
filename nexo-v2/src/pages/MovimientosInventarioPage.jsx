4ec1e627488abd714a221a8c3b54844a684f7541
elcesar <capintof@hotmail.com>
2026-09-10 11:44:36 -0300

    feat(inventory): add inventory movements page

---
+++ nexo-v2/src/pages/MovimientosInventarioPage.jsx
@@ -0,0 +1,5 @@
+import InventoryOperationsPage from '../components/inventory/InventoryOperationsPage.jsx'
+
+export default function MovimientosInventarioPage() {
+  return <InventoryOperationsPage mode="movements" title="Movimientos de inventario" description="Registra ingresos, egresos, traslados y ajustes con trazabilidad por recurso y bodega." />
+}
