13b18469ffa57ddda9d08f1d169f23fab875f833
elcesar <capintof@hotmail.com>
2026-09-10 01:18:04 -0300

    activar estándar global de encabezados internos

--- nexo-v2/src/main.jsx
+++ nexo-v2/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/tokens.css'
 import './styles/components.css'
+import './styles/page-header.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
